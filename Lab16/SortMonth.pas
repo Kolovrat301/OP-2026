e54e1f7a0ac5b8f4acfc4d3cd7389ab448b25ca8
Kolovrat <lyzinivan0@gmail.com>
2026-09-14 11:21:54 +0300

    Add files via upload

---
+++ Lab16/SortMonth.pas
@@ -0,0 +1,29 @@
+PROGRAM SortMonth(INPUT, OUTPUT);
+USES DateIO;
+VAR
+  Mo1, Mo2: Month;
+BEGIN
+  ReadMonth(INPUT, Mo1);
+  ReadMonth(INPUT, Mo2);
+  IF (Mo1 = NoMonth) OR (Mo2 = NoMonth) 
+  THEN
+    WRITELN('¬ходные данные записаны неверно')
+  ELSE 
+    IF  Mo1 = Mo2
+    THEN
+      BEGIN
+        WRITE('ќба мес€ца ');
+        WriteMonth(OUTPUT, Mo1);
+        WRITELN
+      END
+    ELSE
+      BEGIN
+        WriteMonth(OUTPUT, Mo1);
+        IF Mo1 < Mo2
+        THEN 
+          WRITE(' предшествует ')                               
+        ELSE
+          WRITE(' следует за '); 
+        WriteMonth(OUTPUT, Mo2)
+      END  
+END.
